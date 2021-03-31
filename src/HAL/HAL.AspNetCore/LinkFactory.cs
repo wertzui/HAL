cb2dc58fa2f540e6a599e08b69587fa8c725b16a
wertzui <wertzui@spielebeet.de>
2021-03-31 22:06:29 +0200

    Link generation fixed

--- src/HAL/HAL.AspNetCore/LinkFactory.cs
+++ src/HAL/HAL.AspNetCore/LinkFactory.cs
@@ -304,7 +304,7 @@ namespace HAL.AspNetCore
                         sb.Append(',');
                     }
 
-                    sb.Append(parameter.Name);
+                    sb.Append(parameter.BindingInfo.BinderModelName ?? parameter.Name);
                 }
             }
 
