fa986280b3cb7a8a03a0026328f1e64f351d6f0b
Andre Hähnel <andre.haehnel@guenstiger.de>
2022-09-26 14:55:54 +0200

    Added PrompDisplayType

---
+++ src/HAL/HAL.Common/Forms/PropertyPromptDisplayType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAL.Common.Forms
+{
+    /// <summary>
+    /// The display behavior of the property prompt. 
+    /// </summary>
+    public enum PropertyPromptDisplayType
+    {
+        /// <summary>
+        /// The prompt is visible to the user.
+        /// </summary>
+        Visible,
+
+        /// <summary>
+        /// The prompt is invisible to the user but is still taken into account during layouting.
+        /// </summary>
+        Hidden,
+
+        /// <summary>
+        /// The prompt is invisible to the user and doesn't affect the layout.
+        /// </summary>
+        Collapsed,
+    }
+}
